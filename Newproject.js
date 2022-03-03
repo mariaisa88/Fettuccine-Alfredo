df93a48726266c1e220265640c5c0f9bc9e2886a
mariaisa88 <97982419+mariaisa88@users.noreply.github.com>
2022-03-03 12:32:44 -0500

    Add files via upload

---
+++ Newproject.js
@@ -0,0 +1,74 @@
+$(function() {
+  $( "#draggable" ).draggable({stack: 'div'});
+  $('#draggable').addClass('shadow');
+
+});
+
+  $(function() {
+$( "#draggable2" ).draggable({stack: 'div'});
+$('#draggable2').addClass('shadow');
+
+});
+
+  $(function() {
+$( "#draggable3" ).draggable({stack: 'div'});
+$('#draggable3').addClass('shadow');
+
+});
+
+$(function() {
+$( "#draggable4" ).draggable({stack: 'div'});
+$('#draggable4').addClass('shadow');
+
+});
+
+$(function() {
+$( "#draggable5" ).draggable({stack: 'div'});
+$('#draggable5').addClass('shadow');
+
+});
+
+$(function() {
+$( "#draggable6" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable7" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable8" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable9" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable10" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable11" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable12" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable13" ).draggable({stack: 'div'});
+
+});
+
+$(function() {
+$( "#draggable14" ).draggable({stack: 'div'});
+
+});
